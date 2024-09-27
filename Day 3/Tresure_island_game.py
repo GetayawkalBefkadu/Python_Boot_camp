22c1e95281393ccb15d5a3d411b8e0e51413705d
Getayawkal <113996635+GetayawkalBefkadu@users.noreply.github.com>
2024-09-27 08:51:46 -0900

    Update Tresure_island_game.py

--- Day 3/Tresure_island_game.py
+++ Day 3/Tresure_island_game.py
@@ -1,22 +1,47 @@
-# insert the ascii code when there is internet connection
-print("Welcome to Treasure Island!")
-print("Your mission is to find the Treasure.")
-choise1 = input(" you\'r at a crossed, where do you want to go? type 'left' or 'right' ").lower()
-if choise1 == "left":
-    choise2 = input('you have come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.').lower()
-    if choise2 == "wait":
-        chiose3 = input('you arrive at the island unharmed. There is a house with 3 doors. one red, one yellow one blue. Which color do you choose? ').lower()
-        if chiose3 == 'red':
-            print("it is full of fire. Game over!!!")
-        elif chiose3 == 'yellow':
-            print("You find the treasure!!! You win!!")
-        elif chiose3 == 'blue':
-            print("you enter room of beasts!!! Game over.")
-        else:
-            print("you choose a door that doesnt exist!!! Game over.")
-    else:
-        print("you got attacked by shark!! game over!!")
-else:
-    print("you fell in to a hole. Game over!!!")
-# needs to debug not finished!!!
+#  use ascii.co.uk/art for more ascii arts for free!!!
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
 
+#Write your code below this line 👇
+
+choice1 = input('You\'re at a cross road. Where do you want to go? Type "left" or "right" \n').lower()
+if choice1 == "left":
+  choice2 = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. \n').lower()
+  if choice2 == "wait":
+    choice3 = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? \n").lower()
+    if choice3 == "red":
+      print("It's a room full of fire. Game Over.")
+    elif choice3 == "yellow":
+      print("You found the treasure! You Win!")
+    elif choice3 == "blue":
+      print("You enter a room of beasts. Game Over.")
+    else:
+      print("You chose a door that doesn't exist. Game Over.")
+  else:
+    print("You get attacked by an angry trout. Game Over.")
+else:
+  print("You fell into a hole. Game Over.")
+
+  # Write your code below this line 👇
