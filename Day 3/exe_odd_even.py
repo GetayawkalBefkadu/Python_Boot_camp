eea23360789c66b7a053c63c0de79c55c4b6e057
Getayawkal <113996635+GetayawkalBefkadu@users.noreply.github.com>
2024-09-27 06:56:12 -0900

    Update exe_odd_even.py

--- Day 3/exe_odd_even.py
+++ Day 3/exe_odd_even.py
@@ -1,5 +1,8 @@
+# identify even and odd numbers!!
+print("++++ wellcome to even or odd identification world!! ")
 num = float(input("enter any number: \n"))
 if num % 2 == 0:
-    print(f"{num} is even")
+    print(f" The nuber you enter :{num} is even number")
 else:
-    print(f"{num} is odd")
+    print(f" The nuber you enter :{num} is odd number")
+print("now let us try again!!")
