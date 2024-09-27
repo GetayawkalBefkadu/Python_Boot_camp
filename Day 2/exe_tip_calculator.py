a9360bf4aae0bd2ea5033a51f16f60f307091003
Getayawkal <113996635+GetayawkalBefkadu@users.noreply.github.com>
2024-09-27 03:59:50 -0900

    Update exe_tip_calculator.py

--- Day 2/exe_tip_calculator.py
+++ Day 2/exe_tip_calculator.py
@@ -1,3 +1,4 @@
+# Tip calculator!!
 print("welcome to the tip calculator:")
 bill = float(input("what was the total bill? $"))
 tip = float(input("what percentage would you like to give? 10,12 or 15? "))
@@ -6,3 +7,4 @@ bill_with_tip = tip/100 * bill + bill
 total_bill = bill_with_tip / people
 round(total_bill,2 )
 print(f"your total bill is {total_bill} each!!")
+print("Thank You for your kindness!!")
