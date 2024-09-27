141c845bb5f909491e9e30940fee71d98d6038b9
Getayawkal <113996635+GetayawkalBefkadu@users.noreply.github.com>
2024-09-27 03:43:46 -0900

    Update exe_BMI.py

--- Day 2/exe_BMI.py
+++ Day 2/exe_BMI.py
@@ -1,4 +1,13 @@
+# Calculating BMI(Body mass index)
 height = float(input("insert your height \n"))
 weight = float(input("insert your weight \n"))
 BMI = weight / (height * height)
-print(BMI)
+if 18 <= BMI <= 25:
+    print("You Body Mass Index is:  " + str(BMI) + "and it is normal!!")
+elif BMI < 18:
+    print("You Body Mass Index is:  " + str(BMI) + "and it is low BMI!!")
+elif BMI > 25:
+    print("You Body Mass Index is:  " + str(BMI) + "and high BMI You should see a doctor or work some workout!! ")
+else:
+    print("Inter a correct information please !!")
+
