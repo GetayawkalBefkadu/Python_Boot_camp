35b25891c87291c8b435442b246cbb16d006f561
Getayawkal <113996635+GetayawkalBefkadu@users.noreply.github.com>
2024-09-30 10:14:20 -0900

    Update Randomizaton.py

--- Day 4/Randomizaton.py
+++ Day 4/Randomizaton.py
@@ -1,5 +1,8 @@
-# when i use random for the first time my file name was random.py
+# when i use random (built in)  for the first time my file name was random.py
 # which results name conflict and take me   a long time to find out the error!!
 import random
 random_int = random.randint(1,100)
 print(f"i am just generating random integer:- {random_int}")
+print("=================================================")
+random_float = random.random() # this will generate random number but will not take any argument!! 
+print(f"i am just generating random integer:- {random_float}")
