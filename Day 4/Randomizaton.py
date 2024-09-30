b89cd4071d91e85b04c097a20751ed952800d687
Getayawkal <113996635+GetayawkalBefkadu@users.noreply.github.com>
2024-09-30 10:10:05 -0900

    Update Randomizaton.py

--- Day 4/Randomizaton.py
+++ Day 4/Randomizaton.py
@@ -1,8 +1,5 @@
+# when i use random for the first time my file name was random.py
+# which results name conflict and take me   a long time to find out the error!!
 import random
-import my_modulue
-random_integer = random.randint(1, 10)
-# print(random_integer)
-# now let us create  module and see to import it and how to use it.
-# print(my_modulue.pi)
-random_float = random.random()
-print(round(random_float))
+random_int = random.randint(1,100)
+print(f"i am just generating random integer:- {random_int}")
