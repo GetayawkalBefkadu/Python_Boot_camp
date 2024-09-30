75732f3c78b1ee3568db4effeec3dc82ebaa72f9
Getayawkal <113996635+GetayawkalBefkadu@users.noreply.github.com>
2024-09-30 10:37:01 -0900

    Add files via upload

---
+++ Day 4/Tose_eg.py
@@ -0,0 +1,17 @@
+import random
+print("-----Wellcome to Head or tail game--------")
+python_pt = "pythonizer"
+print(f"my name is: {python_pt}" )
+gamer = input("what is your name my friend? \n")
+print(f"well wellcome dear: {gamer}")
+print("Now choose Head or tail?")
+gamer = input()
+random_gusse = random.randint(0,1 )
+if random_gusse == 0:
+    print("Head")
+else:
+    print("Tail")
+if random_gusse == gamer:
+    print("same choise no winner")
+elif random_gusse != gamer:
+    print("You win")
