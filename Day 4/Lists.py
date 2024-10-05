82d19e80aabdaff35b400fef78ecbd78b08af2df
Getayawkal <113996635+GetayawkalBefkadu@users.noreply.github.com>
2024-10-04 23:39:15 -0900

    Update Lists.py

--- Day 4/Lists.py
+++ Day 4/Lists.py
@@ -1,3 +1,4 @@
+# List us  see lists data structure  in python
 Regions_of_Ethiopia = ["Addis ababa", "Harari", "Dera dewa", "Oromia", "Tigeray", "Amhara", "Gumuz", "Guragea"]
 print(Regions_of_Ethiopia[-2])
 Regions_of_Ethiopia.append("Afar")
