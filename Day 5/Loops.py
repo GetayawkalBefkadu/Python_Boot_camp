d02a31e4ad173ce0354e1feda756c12e78050c91
Getayawkal Befkadu <113996635+GetayawkalBefkadu@users.noreply.github.com>
2024-10-11 09:14:02 -0900

    Update Loops.py

--- Day 5/Loops.py
+++ Day 5/Loops.py
@@ -1,5 +1,7 @@
 # let us see how we can use loop in python
+print("which programing language should i learn? \n")
 Programing = ["Python", "JAVA", "JavaScript", "Dart"]
 for programmer in Programing:
-    print(programmer)
+    print(programmer) # we can also print a lot of things under the loop. well lets try some examples below
+    print( "is " + programmer + " better?")
 
