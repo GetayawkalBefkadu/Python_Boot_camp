e6f39fa23ff1b012828d0630083dc395b07e1b7c
Getayawkal Befkadu <113996635+GetayawkalBefkadu@users.noreply.github.com>
2024-10-11 09:20:54 -0900

    Update Loops.py

--- Day 5/Loops.py
+++ Day 5/Loops.py
@@ -3,5 +3,7 @@ print("which programing language should i learn? \n")
 Programing = ["Python", "JAVA", "JavaScript", "Dart"]
 for programmer in Programing:
     print(programmer) # we can also print a lot of things under the loop. well lets try some examples below
-    print( "is " + programmer + " better?")
+    print( "🖥  is " + programmer + " better?")
+# now we are out-side of the loop so, any-thing under the loop will be printed once.
+print("wooooops!!!, I am confused.")
 
