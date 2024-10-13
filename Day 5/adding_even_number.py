920d994f115805bf89a55ae8c3af34fabd6bf6b8
Getayawkal Befkadu <113996635+GetayawkalBefkadu@users.noreply.github.com>
2024-10-13 09:05:28 -0900

    Update adding_even_number.py

--- Day 5/adding_even_number.py
+++ Day 5/adding_even_number.py
@@ -1,5 +1,5 @@
 # sum of even numbers
 sum_of_even = 0
-for number in range(1,100,2):
+for number in range(2,100,2):
     sum_of_even += number
 print(f"sum of even numbers from 1 to 100 is: {sum_of_even}")
