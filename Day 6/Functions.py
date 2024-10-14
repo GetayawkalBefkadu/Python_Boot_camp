f8f2a5c38c44b422f844c539c88290b6e3bf0146
Getayawkal Befkadu <113996635+GetayawkalBefkadu@users.noreply.github.com>
2024-10-14 10:21:01 -0900

    Update Functions.py

--- Day 6/Functions.py
+++ Day 6/Functions.py
@@ -14,3 +14,17 @@ num = int(input("Enter a number to find its square: "))
 
 # Calling the function with user's input and displaying the result
 print("The square of", num, "is", calculate_square(num))
+# ---------------------------------------------------------------------
+# Function to check if a number is even or odd
+def check_even_odd(number):
+    # If number is divisible by 2 with no remainder, it's even
+    if number % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+# Interactive input: Asking the user to input a number
+num = int(input("Enter a number to check if it's even or odd: "))
+
+# Calling the function and displaying the result
+print(f"The number {num} is {check_even_odd(num)}.")
