e97b51c534fbf4038ee783cc2a610de4a012438d
Getayawkal Befkadu <113996635+GetayawkalBefkadu@users.noreply.github.com>
2024-10-14 10:21:33 -0900

    Update while.py

---
+++ Day 6/while.py
@@ -0,0 +1,15 @@
+# While loop to print numbers from 1 to 5
+# The variable 'counter' is used to control the loop
+counter = 1
+
+# The loop continues as long as 'counter' is less than or equal to 5
+while counter <= 5:
+    # Print the current value of 'counter'
+    print("Current number:", counter)
+
+    # Increment the counter to avoid infinite loop
+    counter += 1
+
+# Once the loop ends, print a message
+print("Loop ended.")
+# ----------------------------------------------------------------
