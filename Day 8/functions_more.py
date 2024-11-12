9e744cf8b2bf4ee13c64479112cfb83da4dedb27
Getayawkal Befkadu <113996635+GetayawkalBefkadu@users.noreply.github.com>
2024-11-12 10:27:16 -0900

    Update functions_more.py

--- Day 8/functions_more.py
+++ Day 8/functions_more.py
@@ -1,5 +1,10 @@
-def greet():
+def greet(): # normal function without parameter
     print("i choose python")
     print("and the javascript")
     print("finally became a software engineer!!!")
 greet()
+# ---------------let us see functions with parameter
+def greeting(name):
+    print(f"hello dear {name}")
+    print(f"Thank you for learning python  {name}")
+greeting("Getayawkal ")
