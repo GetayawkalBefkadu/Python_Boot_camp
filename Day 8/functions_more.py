4e299fb3e74af60a9a2cf5cb7c1f70c71d95c1f4
Getayawkal Befkadu <113996635+GetayawkalBefkadu@users.noreply.github.com>
2024-11-17 21:02:55 +0300

    Update functions_more.py

--- Day 8/functions_more.py
+++ Day 8/functions_more.py
@@ -36,3 +36,16 @@ def Triangle():
     print(f"The are of triangle is : {tri}")
 Triangle()
 # ---------------------------------------------------------------
+def is_a_triangle(a, b, c):
+    return a + b > c and b + c > a and c + a > b
+
+
+a = float(input('Enter the first side\'s length: '))
+b = float(input('Enter the second side\'s length: '))
+c = float(input('Enter the third side\'s length: '))
+
+if is_a_triangle(a, b, c):
+    print('Yes, it can be a triangle.')
+else:
+    print('No, it can\'t be a triangle.')
+    # --------------------------------------------------------------
