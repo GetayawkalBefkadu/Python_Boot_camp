3e3efa0d0f5b05f85336c8f30f0f5440a4938e1b
Getayawkal Befkadu <113996635+GetayawkalBefkadu@users.noreply.github.com>
2024-11-17 20:53:50 +0300

    Update functions_more.py

--- Day 8/functions_more.py
+++ Day 8/functions_more.py
@@ -8,3 +8,23 @@ def greeting(name):
     print(f"hello dear {name}")
     print(f"Thank you for learning python  {name}")
 greeting("Getayawkal ")
+# ------------------------------------------------------------
+def BMI():
+    # Get user input for weight and height
+    weight = float(input("Enter your weight in kilograms (kg): \n"))
+    height = float(input("Enter your height in meters (m): \n"))
+
+    # Calculate BMI
+    bmi = weight / height ** 2
+
+    # Display BMI
+    print(f"Your BMI is {bmi:.2f}")
+
+    # Provide feedback based on BMI value
+    if bmi < 18.5:
+        print("You have a lower BMI. Consider improving your eating habits!")
+    elif 18.5 <= bmi < 25:
+        print("Congrats! Your BMI is normal. Keep it up!")
+    elif bmi >= 25:
+        print("Warning: Your BMI indicates overweight. It's time to start exercising!")
+BMI()
