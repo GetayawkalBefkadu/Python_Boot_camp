b308a6fc24e6c95a2639db88527ea8f5ce405489
Getayawkal Befkadu <113996635+GetayawkalBefkadu@users.noreply.github.com>
2024-11-27 21:26:32 +0300

    Update cesar.py

--- Day 8/cesar.py
+++ Day 8/cesar.py
@@ -1,3 +1,4 @@
+# this ciper text encoding text 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 
             'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 
             'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 
